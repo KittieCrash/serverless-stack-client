998a5299c6a1a727b2d32b861e3ba7c5893df08c
Kittie Durkey <kittie.durkey@alchemysystems.com>
2020-04-12 14:34:26 -0500

    Setting up React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "kitkat-note-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://ywlsxf9n93.execute-api.us-east-1.amazonaws.com/prod/"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_LW6SqXysM",
+      APP_CLIENT_ID: "78s2kstrloi1jdbj2lsm225svr",
+      IDENTITY_POOL_ID: "us-east-1:94de94c2-7541-4c57-ab03-82acb51d6258"
+    }
+  };
